ab255be17f7192498c4b4ca476c89b7c55ef8164
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-30 15:00:07 +0200

    add splice

--- app/src/xtring.ts
+++ app/src/xtring.ts
@@ -1,5 +1,14 @@
+import { constructApplyToPrototype } from "attatch-to-prototype"
 
+export default function(StringConstructor: { new(...a: any): String } = String) {
+  const as = constructApplyToPrototype(StringConstructor.prototype)
 
-export default function() {
-  
+  as("splice", function(index: number, count: number, ...add: string[]) {
+    if (index < 0) {
+      index += this.length;
+      if (index < 0)
+        index = 0;
+    }
+    return this.slice(0, index) + (add.join("") || "") + this.slice(index + count);
+  })
 }
